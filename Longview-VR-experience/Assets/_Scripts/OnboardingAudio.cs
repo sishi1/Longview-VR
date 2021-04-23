e3cffc1af3c339c58e4673f96292ba5a3fd91dba
Sishi <sishi.hu17@gmail.com>
2021-04-23 14:25:32 +0200

    Added some more models/textures

--- Longview-VR-experience/Assets/_Scripts/OnboardingAudio.cs
+++ Longview-VR-experience/Assets/_Scripts/OnboardingAudio.cs
@@ -32,7 +32,7 @@ namespace Valve.VR.InteractionSystem
 
         private IEnumerator PlayAudio()
         {
-            yield return new WaitForSeconds(5);
+            yield return new WaitForSeconds(3);
 
             audioSource.Play();
         }
