c78c54ef557f7e56c57a668011a4fceed01c1d34
Sishi <sishi.hu17@gmail.com>
2021-06-09 18:32:28 +0200

    Gaze is working and fixed dead man's weird shader

--- Longview-VR-experience/Assets/_Scripts/Notebook/MarkObject.cs
+++ Longview-VR-experience/Assets/_Scripts/Notebook/MarkObject.cs
@@ -6,13 +6,13 @@ namespace Valve.VR.InteractionSystem
 {
     public class MarkObject : MonoBehaviour
     {
-        [SerializeField] LineRenderer line;
-        [SerializeField] LayerMask checkLayer;
-
         private Player player = null;
 
         [Header("Miscellaneous")]
         public Transform controller;
+
+        [SerializeField] private LineRenderer line;
+        [SerializeField] private LayerMask checkLayer;
 
         private GameObject selectedObject;
 
@@ -24,13 +24,13 @@ namespace Valve.VR.InteractionSystem
         public SteamVR_Action_Boolean aButton;
         public SteamVR_Action_Vector2 joystickSelection;
         public SteamVR_Action_Boolean trigger;
+        public SteamVR_Action_Boolean grip;
 
         [Header("Hints")]
         [SerializeField]
         private string openSelectionMenuHint;
         [SerializeField]
         private string joystickSelectionHint;
-
 
         [HideInInspector]
         public ObjectState objectState;
@@ -45,7 +45,15 @@ namespace Valve.VR.InteractionSystem
         private bool usedAButton;
         private bool usedJoystick;
         private bool objectInHand = false;
+        private bool objectHit = false;
+        private bool activateGaze = false;
 
+        //Timer en seconds beginnen allebei met 0, want je wilt pas aftellen wanneer we onze raycast wijzen naar een object
+        private float timer = 0f;
+        private int seconds = 0;
+
+        //Deze variabel is anders. Hier heb ik het getal 5 neergezet. activationTime heeft dus een getal waarde van 5
+        private int activationTime = 5;
 
         private void Start()
         {
@@ -54,98 +62,164 @@ namespace Valve.VR.InteractionSystem
             childCountRightHand = player.rightHand.gameObject.transform.childCount + 0;
             childCountLeftHand = player.leftHand.gameObject.transform.childCount + 0;
 
-            maxChildCountRightHand = 12;
+            maxChildCountRightHand = 10;
             maxChildCountLeftHand = 7;
-
         }
 
         private void Update()
         {
-            if (selectedObject != null)
-                Debug.Log($"<b>markobject </b> Object state: {(objectState == null ? "null" : objectState.ToString())} Selected object: {selectedObject.name}");
-
-            Debug.Log($"<b>markobject </b> Right standard child count: {childCountRightHand} Right child count: {player.rightHand.gameObject.transform.childCount}");
-            Debug.Log($"<b>markobject </b> Left standard child count: {childCountLeftHand} Left child count: {player.leftHand.gameObject.transform.childCount}");
-
-            //Checks whether the player has an object in hand
-            if (player.rightHand.gameObject.transform.childCount != childCountRightHand && !objectInHand)
+            if (grip.GetStateDown(hands))
             {
-                //Attach the object in hand in the variable
-                selectedObject = player.rightHand.transform.GetChild(player.rightHand.transform.childCount - 1).gameObject;
-                objectState = selectedObject.GetComponent<ObjectState>();
-                objectInHand = objectState != null;
-
-                if (objectInHand)
-                {
-                    StaticVariables.joystickMovementActive = false;
-                    steamVRTeleport.SetActive(false);
-                    snapTurn.SetActive(false);
-                }
+                if (activateGaze)
+                    activateGaze = false;
+                else
+                    activateGaze = true;
             }
 
-            if (player.leftHand.gameObject.transform.childCount != childCountLeftHand && !objectInHand)
+            if (activateGaze)
             {
-                //Attach the object in hand in the variable
-                selectedObject = player.leftHand.transform.GetChild(player.leftHand.transform.childCount - 1).gameObject;
-                objectState = selectedObject.GetComponent<ObjectState>();
-                objectInHand = objectState != null;
+                line.enabled = true;
 
-                if (objectInHand)
+                //Gaat kijken of er een collision plaatsvindt met een object
+                if (Physics.Raycast(controller.position, controller.forward, out RaycastHit hit, StaticVariables.lineLength, checkLayer))
                 {
-                    StaticVariables.joystickMovementActive = false;
-                    steamVRTeleport.SetActive(false);
-                    snapTurn.SetActive(false);
+                    Debug.Log(timer);
+
+                    //Telt langzamerhand in milliseconden omhoog
+                    timer += Time.deltaTime;
+
+                    //Als de timer variabel 1 seconden heeft bereikt dan gaat het door naar de code in de { }
+                    if (timer >= 1f)
+                    {
+                        //Deze variabel staat iets bovenaan, maar het gaat telkens met 1 omhoog als de if statement hierboven op true staat
+                        seconds++;
+
+                        //De timer wordt teruggezet op 0 zodat het vanaf het begin weer begint met tellen
+                        timer = 0;
+                    }
+
+                    //Zodra de seconden die we hebben geteld gelijk staat aan de variabel activationTime, dan gaan we door met de code in de { }
+                    if (seconds >= activationTime)
+                    {
+                        //selectedObject wordt gelijkgesteld aan het gameobject wat onze raycast heeft gevonden
+                        selectedObject = hit.collider.gameObject;
+
+                        //objectState weet nu welk gameobject (bijvoorbeeld phone) hij naar de notebook kan sturen
+                        objectState = selectedObject.GetComponent<ObjectState>();
+
+                        //Checkt of objectState wel een object heeft of niet
+                        objectHit = objectState != null;
+
+                        //Als objectState een object heeft en dus niet null is, dan gaan we verder in de { }
+                        if (objectHit)
+                        {
+                            //Hier zetten we de selections aan (de layout die Kaj heeft gemaakt)
+                            StaticVariables.activateSelection = true;
+
+                            //Hiermee zorgen we ervoor dat we de layout kunnen selecteren a.d.h.v. onze joystick positie
+                            JoystickSelection();
+
+                            //Dit allemaal zorgt ervoor dat we niet kunnen teleporteren, bewegen en draaien
+                            StaticVariables.joystickMovementActive = false;
+                            steamVRTeleport.SetActive(false);
+                            snapTurn.SetActive(false);
+                        }
+                    }
                 }
-            }
-
-            if ((player.rightHand.transform.childCount == maxChildCountRightHand ||
-                player.leftHand.transform.childCount == maxChildCountLeftHand) && !usedAButton)
-                OpenHint(aButton, openSelectionMenuHint);
-
-            //Two buttons can't be true at the same time, hence why trigger state is false since that's the first button that was activated and becomes false
-            if (aButton.GetStateDown(hands) && !trigger.GetStateDown(hands))
-                if (player.rightHand.transform.childCount == maxChildCountRightHand ||
-                    player.leftHand.transform.childCount == maxChildCountLeftHand)
-                {
-                    usedAButton = true;
-                    StaticVariables.activateSelection = true;
-                }
-
-            if (StaticVariables.activateSelection && objectState != null)
-            {
-                HideHint(aButton);
-
-                if (!usedJoystick)
-                    OpenHint(joystickSelection, joystickSelectionHint);
-
-                JoystickSelection();
-            }
-
-            //Reset everything when you stop holding an object in hand
-            if (!aButton.GetStateDown(hands) && !trigger.GetStateDown(hands))
-                if (player.rightHand.transform.childCount != maxChildCountRightHand &&
-                    player.leftHand.transform.childCount != maxChildCountLeftHand)
+                else
+                    //Als de raycast niet meer met een object collide, dan resetten we alles om ervoor te zorgen dat we "fresh" van start gaan
                     ResetStatus();
+            }
+            else
+            {
+                line.enabled = false;
+
+                if (selectedObject != null)
+                    Debug.Log($"<b>markobject </b> Object state: {(objectState == null ? "null" : objectState.ToString())} Selected object: {selectedObject.name}");
+
+                Debug.Log($"<b>markobject </b> Right standard child count: {childCountRightHand} Right child count: {player.rightHand.gameObject.transform.childCount}");
+                Debug.Log($"<b>markobject </b> Left standard child count: {childCountLeftHand} Left child count: {player.leftHand.gameObject.transform.childCount}");
+
+                //Checks whether the player has an object in hand
+                if (player.rightHand.gameObject.transform.childCount != childCountRightHand && !objectInHand)
+                {
+                    //Attach the object in hand in the variable
+                    selectedObject = player.rightHand.transform.GetChild(player.rightHand.transform.childCount - 1).gameObject;
+                    objectState = selectedObject.GetComponent<ObjectState>();
+                    objectInHand = objectState != null;
+
+                    if (objectInHand)
+                    {
+                        StaticVariables.joystickMovementActive = false;
+                        steamVRTeleport.SetActive(false);
+                        snapTurn.SetActive(false);
+                    }
+                }
+
+                if (player.leftHand.gameObject.transform.childCount != childCountLeftHand && !objectInHand)
+                {
+                    //Attach the object in hand in the variable
+                    selectedObject = player.leftHand.transform.GetChild(player.leftHand.transform.childCount - 1).gameObject;
+                    objectState = selectedObject.GetComponent<ObjectState>();
+                    objectInHand = objectState != null;
+
+                    if (objectInHand)
+                    {
+                        StaticVariables.joystickMovementActive = false;
+                        steamVRTeleport.SetActive(false);
+                        snapTurn.SetActive(false);
+                    }
+                }
+
+                if ((player.rightHand.transform.childCount == maxChildCountRightHand ||
+                    player.leftHand.transform.childCount == maxChildCountLeftHand) && !usedAButton)
+                    OpenHint(aButton, openSelectionMenuHint);
+
+                //Two buttons can't be true at the same time, hence why trigger state is false since that's the first button that was activated and becomes false
+                if (aButton.GetStateDown(hands) && !trigger.GetStateDown(hands))
+                    if (player.rightHand.transform.childCount == maxChildCountRightHand ||
+                        player.leftHand.transform.childCount == maxChildCountLeftHand)
+                    {
+                        usedAButton = true;
+                        StaticVariables.activateSelection = true;
+                    }
+
+                if (StaticVariables.activateSelection && objectState != null)
+                {
+                    HideHint(aButton);
+
+                    if (!usedJoystick)
+                        OpenHint(joystickSelection, joystickSelectionHint);
+
+                    JoystickSelection();
+                }
+
+                //Reset everything when you stop holding an object in hand
+                if (!aButton.GetStateDown(hands) && !trigger.GetStateDown(hands))
+                    if (player.rightHand.transform.childCount != maxChildCountRightHand &&
+                        player.leftHand.transform.childCount != maxChildCountLeftHand)
+                        ResetStatus();
+            }
         }
 
         private void JoystickSelection()
         {
-            //left/interesting
+            //left/ask specialist
             if (joystickSelection.axis.x >= -1f && joystickSelection.axis.x < -0.2f && joystickSelection.axis.y >= -0.71f && joystickSelection.axis.y < 0.71f)
+            {
+                selection = "specialist";
+                usedJoystick = true;
+            }
+            //top/interesting
+            else if (joystickSelection.axis.x >= -0.71f && joystickSelection.axis.x <= 0.71f && joystickSelection.axis.y > 0.2f && joystickSelection.axis.y <= 1f)
             {
                 selection = "interesting";
                 usedJoystick = true;
             }
-            //top/confiscate
-            else if (joystickSelection.axis.x >= -0.71f && joystickSelection.axis.x <= 0.71f && joystickSelection.axis.y > 0.2f && joystickSelection.axis.y <= 1f)
-            {
-                selection = "confiscate";
-                usedJoystick = true;
-            }
-            //right/ask specialist
+            //right/confiscate
             else if (joystickSelection.axis.x <= 1f && joystickSelection.axis.x > 0.2f && joystickSelection.axis.y >= -0.71f && joystickSelection.axis.y < 0.71f)
             {
-                selection = "specialist";
+                selection = "confiscate";
                 usedJoystick = true;
             }
             //bottom/nothing
@@ -210,7 +284,11 @@ namespace Valve.VR.InteractionSystem
         {
             selection = "";
             objectInHand = false;
-            StaticVariables.activateSelection = false;
+            timer = 0;
+            seconds = 0;
+
+            if (StaticVariables.activateSelection)
+                StaticVariables.activateSelection = false;
 
             if (StaticVariables.locomotionStatus == "teleport")
             {
